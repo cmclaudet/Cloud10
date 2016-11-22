1d09b63eb2c86232aed907aa56f951e9c4882602
lfbusta <lfbusta@gmail.com>
2016-11-22 22:58:55 +0100

    Successful Test
    • Successfully implemented random clouds on jsPlayground.html.

--- js/test.js
+++ js/test.js
@@ -1,11 +1,51 @@
-function test(){
-   var left = generateRandom();
-   confirm("asdf");
+/* ============================== */
+/*              MAIN              */
+/* ============================== */
+
+function placeRandomCloud(tagName){
+   target = document.getElementsByTagName(tagName);
+   pos = Math.random()*100 + "%"
+
+   var img = new Image();
+   img.src = "assets/cloudWhite_64_2.svg";
+   img.className = "cloudling";
+   img.alt = "10";
+   img.style.top = pos
+
+   target[0].appendChild(img);
+   console.log("Added cloud at: " + pos);
 }
-function showLetter() {
-    var letter = imgsArray[generateRandomForArray()];
-    $("div").append("<img src='GameHTML5/images/" + letter + ".png'>");
-    var left = generateRandom();
-    var top = generateRandom();
-    $("div").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
+
+function removeCloud(className){
+   target = document.getElementsByClassName(className)[0];
+   target.parentNode.removeChild(target);
+
+   console.log("Removed Cloud.");
+}
+
+function cloudRate(tagName,className,limit){
+   count = document.getElementsByClassName(className).length;
+   if(count >= limit){
+      removeCloud(className);
+      count -= 1;
+   }
+   if(count < limit){
+      placeRandomCloud(tagName)
+   }
+}
+
+/* ============================== */
+/*             ONLOAD             */
+/* ============================== */
+
+window.onload = function(){
+   setInterval("cloudRate('header','cloudling',10)",1000);
+}
+
+/* ============================== */
+/*              TEST              */
+/* ============================== */
+
+function test(){
+   console.log("This is a Test!");
 }
