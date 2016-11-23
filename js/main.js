7fce548df4d953696996f47846d46590b4775e45
lfbusta <lfbusta@gmail.com>
2016-11-23 21:50:21 +0100

    Golden Cloud
    • Added a golden cloud that rushes through the screen every 30 seconds.
    • Removed old files.

--- js/main.js
+++ js/main.js
@@ -33,35 +33,41 @@ function cloudRate(tagName,className,limit){
       placeRandomCloud(tagName)
    }
 
-   console.log("Cloud Count: " + count)
+   //console.log("Cloud Count: " + count)
 }
-/*
+
 function placeGoldenCloud(tagName){
    target = document.getElementsByTagName(tagName);
-   pos = Math.random()*100 + "%"
+   pos = Math.random()*75 + "%"
 
    var img = new Image();
-   img.src = "assets/cloudWhite_64.svg";
+   img.src = "assets/cloudGold_64.svg";
    img.className = "goldenCloud";
    img.alt = "10";
    img.style.top = pos
 
    target[0].appendChild(img);
-   //console.log("Added cloud at: " + pos);
+   console.log("Added cloud at: " + pos);
 }
-*/
+
+function goldenCloudRate(tagName,className,limit){
+   count = document.getElementsByClassName(className).length;
+   if(count >= limit){
+      removeCloud(className);
+      count -= 1;
+   }
+   if(count < limit){
+      placeGoldenCloud(tagName)
+   }
+
+   //console.log("Cloud Count: " + count)
+}
+
 /* ============================== */
 /*             ONLOAD             */
 /* ============================== */
 
 window.onload = function(){
    setInterval("cloudRate('body','cloudling',30)",333);
-}
-
-/* ============================== */
-/*              TEST              */
-/* ============================== */
-
-function test(){
-   console.log("This is a Test!");
+   setInterval("goldenCloudRate('body','goldenCloud',1)",30000);
 }
